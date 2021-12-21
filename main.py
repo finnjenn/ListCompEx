895f988fa95b1b90a3850f3a4b1059d33f21d434
Finn Jennings <85904957+finnjenn@users.noreply.github.com>
2021-12-21 23:14:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+#list of author names
+#the problem asks us to create a list of all the author names by using split with ',' as its argument
+
+author_names = authors.split(',') #creates list of author names and prints them
+print(author_names)
+
+#Now they ask us to create a list that only contains the last names for each author
+#Normally we would just loop through the previous list, split each name into first and last, and append the last name to a new list
+#I have essentially done all of that in a single line below
+author_last_names = [x.split()[-1] for x in author_names]
+#My translation(sorry):
+# For x in author_names, we split that author name into [first,last]. the negative 1 index accesses the ending index of the [first,last] list and adds it to author_last_names
+print('\n\n')
+print(author_last_names)
