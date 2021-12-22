b44256e77bebb3573b877f5e606016e593beac08
Finn Jennings <finnjennings00@gmail.com>
2021-12-22 05:54:32 +0000

    Added a new list comp example

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ author_last_names = [x.split()[-1] for x in author_names]
 # For x in author_names, we split that author name into [first,last]. the negative 1 index accesses the ending index of the [first,last] list and adds it to author_last_names
 print('\n\n')
 print(author_last_names)
+
+authorlast = [x[x.find(' ')+1:] for x in author_names]
+#here is another list comprehension that completes the same task, but shows how you can use methods, slicing, and indexing all at once with list comprehension
+#this one is very confusing to explain
+#x.find(' ') returns the index of the argument ' '. adds one to get index of the beginning of the last name 
+#slices from that index all the way to the end of the string and finally adding it to the list
+print (authorlast)
